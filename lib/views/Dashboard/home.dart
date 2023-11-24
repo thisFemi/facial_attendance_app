f135d90de8bf4c2da2e36a9c6a68abbf8b8c6bf2
Oluwafemi <oluwafemijacob1@gmail.com>
2023-11-24 18:11:39 +0100

    home done

--- lib/views/Dashboard/home.dart
+++ lib/views/Dashboard/home.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 import '../../utils/Common.dart';
 import '../../utils/colors.dart';
@@ -13,61 +14,79 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(top: 40, left: 20, right: 20),
+      padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
       width: Screen.deviceSize(context).width,
       color: AppColors.white,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               ClipRRect(
                 borderRadius: BorderRadius.circular(
                     Screen.deviceSize(context).height * .03),
                 child: CachedNetworkImage(
-                  height: Screen.deviceSize(context).height * .05,
+                  color: AppColors.black,
                   width: Screen.deviceSize(context).height * .05,
                   fit: BoxFit.cover,
                   imageUrl: "APIs.userInfo.image",
-                  errorWidget: (context, url, error) => CircleAvatar(
-                    child: Icon(CupertinoIcons.person),
+                  errorWidget: (context, url, error) => const CircleAvatar(
+                    backgroundColor: AppColors.black,
+                    child: Icon(
+                      CupertinoIcons.person,
+                      color: AppColors.white,
+                    ),
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 width: 10,
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(
-                    width: Screen.deviceSize(context).width / 2,
-                    child: Text(
-                      'Hello ${"APIs.userInfo.name"} 🎉',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                    ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: Screen.deviceSize(context).width / 2.7,
+                        child: const Text(
+                          'Hello, ${"APIs.userInfo.name"}',
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                      ),
+                      const Text(
+                        '🎉',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                    ],
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 5,
                   ),
-                  Text(
+                  const Text(
                     'Be on time!',
                     style: TextStyle(
                         color: AppColors.grey,
                         fontWeight: FontWeight.bold,
                         fontSize: 13),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
                 ],
               ),
-              Spacer(),
+              const Spacer(),
               InkWell(
                 onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => NotificationScreen()));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const NotificationScreen()));
                 },
                 child: NotificationIcon(
                   iconData: Icons.notifications_none_rounded,
@@ -77,12 +96,71 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
           Card(
-            margin: EdgeInsets.only(top: 10),
+            margin: const EdgeInsets.only(top: 10),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 3,
             child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all()),
-              child: Column(),
+              height: Screen.deviceSize(context).height / 5,
+              width: Screen.deviceSize(context).width,
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircularPercentIndicator(
+                    radius: Screen.deviceSize(context).height * .08,
+                    curve: Curves.linear,
+                    animation: true,
+                    animationDuration: 1200,
+                    lineWidth: 15.0,
+                    circularStrokeCap: CircularStrokeCap.round,
+                    percent: 0.50,
+                    center: new Text(
+                      "50%",
+                      style: new TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                    backgroundColor: AppColors.lightGrey,
+                    progressColor: AppColors.black,
+                  ),
+                  const Spacer(),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Overall Stats',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            color: AppColors.algaeBrown,
+                            size: 18,
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 5.0),
+                            child: SizedBox(
+                              child: Text(
+                                "Total Courses: 45",
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
